29d8b7b473a00ac4607a68d3faa21655a1ea9796
Alan White <alannmsu@gmail.com>
2019-09-13 18:16:49 -0400

    More css fixes

--- js/advancedRoll.js
+++ js/advancedRoll.js
@@ -7,9 +7,18 @@ let advancedRollSubmit = document.getElementById('advanced-roll-submit');
 
 //The onClick handler for advanced rolls
 let advancedRoll = () => {
-	var final = finalCut().join(', ');
+
+	var form = document.getElementById('advanced-roll');
+	var isValidForm = form.checkValidity();
+
+	if(isValidForm == true) {
+		var final = finalCut().join(', ');
 	let advancedResults = document.getElementById('advanced-results');
 	advancedResults.innerHTML = final;
+	} else {
+		console.log('error');
+	}
+
 };
 
 
