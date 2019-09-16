a3be0f23a12ded2fcfb05b51dd8483196214469a
Alan White <alannmsu@gmail.com>
2019-09-16 15:23:42 -0400

    Add tooltips via jQuery and Bootstrap

--- js/advancedRoll.js
+++ js/advancedRoll.js
@@ -1,7 +1,6 @@
 import finalCut from './finalCut.js';
 import valOf from './valOf.js';
 
-
 //ADVANCED ROLL LOGIC
 let advancedRollSubmit = document.getElementById('advanced-roll-submit');
 //The onClick handler for advanced rolls
